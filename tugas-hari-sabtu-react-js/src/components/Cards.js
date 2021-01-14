d3f1cb7b44fa7903a08eb859ccc48fb16237f050
Prieyudha Akadita S <akaditasustono@gmail.com>
2021-01-14 21:19:57 +0700

    add image support

--- tugas-hari-sabtu-react-js/src/components/Cards.js
+++ tugas-hari-sabtu-react-js/src/components/Cards.js
@@ -10,33 +10,31 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src={require('../assets/images/img-3.jpg')}
+              src={'/images/img-3.jpg'}
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
               label='Adventure'
-              path='/services'
-            />
+              path='/services'/>
             <CardItem
-              src={require('../assets/images/img-3.jpg')}
+              src={'/images/img-3.jpg'}
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
-              path='/services'
-            />
+              path='/services'/>
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src={require('../assets/images/img-3.jpg')}
+              src={'/images/img-3.jpg'}
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src={require('../assets/images/img-3.jpg')}
+              src={'/images/img-3.jpg'}
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src={require('../assets/images/img-3.jpg')}
+              src={'/images/img-3.jpg'}
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path='/sign-up'
